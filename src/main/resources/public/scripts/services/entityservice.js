3b29bfab5e5c81cb0012efad2f6422cd399b2576
runapal5 <runapal5@gmail.com>
2017-02-10 12:52:08 +0530

    Add files via upload

--- src/main/resources/public/scripts/services/entityservice.js
+++ src/main/resources/public/scripts/services/entityservice.js
@@ -9,22 +9,19 @@
  */
 
 angular.module('storeClientApp').factory('dataFactory', ['$http', function ($http){
-	//var urlBase = 'http://localhost:8080/';
+
 	var urlBase = 'http://172.18.51.87:81/devcapsule-spring-1.0/';
-	//var urlBase ='/';
-	//var urlBase = '';
+	
 	var dataFactory = {};
 
 	//Find All - Valid for all entities
 	dataFactory.findAll = function(entity) {
-		alert("entity:::"+urlBase + entity);
+		//alert("entity:::"+urlBase + entity);
 		var promise = $http({method:'GET', url: urlBase + entity})
-			.success(function (data) {
-				alert("data:::"+data);
+			.then(function (data) {
 				return data;
-			})
-			.error(function () {
-				alert("data:::No ***");
+			},
+			function () {
 				return {'status': "Unable to get all bases"};
 			});
 
@@ -34,13 +31,14 @@ angular.module('storeClientApp').factory('dataFactory', ['$http', function ($htt
 	//Add New - Valid for all entities
 	dataFactory.addEntity = function(entity, data) {
 		var promise = $http({method:'POST', url: urlBase + entity, data: data, headers: {'Content-Type': 'application/json'}})
-			.success(function (data) {
+			.then(function (data) {
 				return data;
-			})
-			.error(function () {
+			},
+			function () {
 				return {'status': "Entity could not be added"};
 			});
 
+			//Entity could not be added
 		return promise;
 	};
 
@@ -48,12 +46,14 @@ angular.module('storeClientApp').factory('dataFactory', ['$http', function ($htt
 	dataFactory.editEntity = function(entity, id, data) {
 		var promise = $http({method: 'POST', url: urlBase + entity + '/' + id, data: data,
 			headers: {'Content-Type': 'application/json'}})
-		.success(function(data){
-			return data;
-		})
-		.error(function(){
-			return {'status': "Entity could not be edited"};
-		});
+		    .then(function (data) {
+				return data;
+			},
+			function () {
+				return {'status': "Entity could not be edited"};
+			});
+		
+		//Entity could not be edited
 		return promise;
 	};
 
@@ -61,11 +61,15 @@ angular.module('storeClientApp').factory('dataFactory', ['$http', function ($htt
 	dataFactory.deleteEntity = function(entity, id){
 		var promise = $http({method: 'DELETE', url: urlBase + entity + '/' + id,
 			headers: {'Content-Type': 'application/json'}})
-		.success(function(){
-		})
-		.error(function(){
-			return {'status': "Entity could not be deleted"};
-		});
+		.then(function (data) {
+				return data;
+			},
+			function () {
+				return {'status': "Entity could not be deleted"};
+			});
+		
+		//Entity could not be deleted
+		
 		return promise;
 	};
 
