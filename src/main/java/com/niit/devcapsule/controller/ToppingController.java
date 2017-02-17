d9425a50043eab7e05dcc6f8e2bfd2511c07f94e
runapal5 <runapal5@gmail.com>
2017-02-17 13:13:53 +0530

    Add files via upload

--- src/main/java/com/niit/devcapsule/controller/ToppingController.java
+++ src/main/java/com/niit/devcapsule/controller/ToppingController.java
@@ -36,7 +36,7 @@ public class ToppingController {
   ToppingService toppingService;
 
   private static final String loggerMessage = "TrackingId:99a80896-35a4-468c-9ec3-b762ab161429|ClientId:99a80897-35a4-468c-9ec3-b762ab161429"+"|Transaction:{}" 
-		                                      + "|ToppingName :{}"  ; 
+		                                      + "|ToppingName:{}"  ; 
   
   private static final String[] logParameters = new String[6] ;
   
