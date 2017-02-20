7be06869e00d4fa616359040313ad0bb3dd1c86a
runapal5 <runapal5@gmail.com>
2017-02-20 13:43:13 +0530

    Add files via upload

--- src/main/java/com/niit/devcapsule/DevcapsuleSpringApplication.java
+++ src/main/java/com/niit/devcapsule/DevcapsuleSpringApplication.java
@@ -24,7 +24,7 @@ public class DevcapsuleSpringApplication extends SpringBootServletInitializer {
 	
  	
   public static void main(String[] args) {
-    SpringApplication.run(DevcapsuleSpringApplication.class, args);
+    SpringApplication.run(DevcapsuleSpringApplication.class, args).close();
   }
  
     @Override
